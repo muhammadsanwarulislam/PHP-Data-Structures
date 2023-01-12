32f401f9939bcb6f3ba60ce04fd298154018ec45
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-01-12 19:53:49 +0600

    Working on array

--- 01_Array/OPP/PracticeWithArray.php
+++ 01_Array/OPP/PracticeWithArray.php
@@ -1,23 +1,27 @@
 <?php
 
 class PracticeWithArray {
-    public $numericArray = [];
-    public $multiDimensionalArray = [];
+    public $numericArray            = [];
+    public $multiDimensionalArray   = [];
+    public $associativeArrayForInfo = [];
 
-    public function __construct($numericArray, $multiDimensionalArray)
+    public function __construct($numericArray, $multiDimensionalArray, $associativeArrayForInfo)
     {
         $this->numericArray             =   $numericArray;
         $this->multiDimensionalArray    =   $multiDimensionalArray;
+        $this->associativeArrayForInfo  =   $associativeArrayForInfo;
     }
 
-    public function outputOfTheNumericArray() {
+    public function outputOfTheNumericArray(): PracticeWithArray
+    {
         foreach($this->numericArray as $key => $number){
             echo "The position " .$key. " holds the value " .$number. "\n";
         }
+        return $this;
     }
 
-    public function outputOfTheMultiDimensionalArray() {
-        
+    public function outputOfTheMultiDimensionalArray(): PracticeWithArray 
+    {
         foreach($this->multiDimensionalArray as $key => $values) {
             echo "Info of the player # ".($key + 1). "\n";
             foreach($values as $index => $info){
@@ -25,5 +29,14 @@ class PracticeWithArray {
             }
             echo "\n";
         }
+        return $this;
+    }
+
+    public function outputOfAssociativeArray(): PracticeWithArray
+    {
+        foreach($this->associativeArrayForInfo as $index => $info) {
+            echo $index." : ".$info."\n";
+        }
+        return $this;
     }
 }
--- 01_Array/OPP/index.php
+++ 01_Array/OPP/index.php
@@ -1,13 +1,31 @@
 <?php
+/* 
+ * Example code for: Array OPP
+ * 
+ * Author: Muhammad Sanwarul Islam <muhammad.sanwarul94@gmail.com>
+ * 
+ */
 
 include_once '../OPP/PracticeWithArray.php';
 
-$players    = [];
-$players[]  = ['name' => 'Ronaldo', 'Age' => 37, 'country' => 'Portugal'];
-$players[]  = ['name' => 'Messi', 'Age' => 35, 'country' => 'Argentina'];
-$players[]  = ['name' => 'Neymer', 'Age' => 28, 'country' => 'Brazil'];
+$numeric = [1,2,5,8,45];
 
-$objOfArrayClass = new PracticeWithArray([1,2,3,4],$players);
-$objOfArrayClass->outputOfTheNumericArray();
-$objOfArrayClass->outputOfTheMultiDimensionalArray();
+$associativeArrayForInfo = [];
+$associativeArrayForInfo['name']    = 'muhammad';
+$associativeArrayForInfo['email']   = 'muhammad@gmail.com';
+$associativeArrayForInfo['id']      =  '143311010';
+
+$multiDimensional    = [];
+$multiDimensional[]  = ['name' => 'Ronaldo', 'Age' => 37, 'country' => 'Portugal'];
+$multiDimensional[]  = ['name' => 'Messi', 'Age' => 35, 'country' => 'Argentina'];
+$multiDimensional[]  = ['name' => 'Neymer', 'Age' => 28, 'country' => 'Brazil'];
+
+
+$className = 'PracticeWithArray';
+
+//Using method chaining
+$objOfArrayClass = (new $className($numeric,$multiDimensional, $associativeArrayForInfo))
+                    ->outputOfTheNumericArray()
+                    ->outputOfTheMultiDimensionalArray()
+                    ->outputOfAssociativeArray();
 
