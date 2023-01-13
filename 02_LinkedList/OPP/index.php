95703c98605bfdcf39f86f91abe0a090a211e2ce
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-01-13 23:22:22 +0600

    working on linkedlist

--- 02_LinkedList/OPP/index.php
+++ 02_LinkedList/OPP/index.php
@@ -13,11 +13,8 @@ $nodeObject = (new LinkedList())
 
                 ->insertBefore('Working','PHP OPP')
                 ->display();
-<<<<<<< HEAD
 
 // $obj = new LinkedList();
 // $obj->inserAtFirst('woking 2');
 // $obj->inserAtFirst('woking 3');
 // echo "2nd Item is: ".$obj->getNthNode(2)->data;
-=======
->>>>>>> 3e9221e0809fad2b571ca79ba7107f8a472e02c0
