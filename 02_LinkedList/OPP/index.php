d8a17e8f71c10565de9bf6f107f18c6bdd9c8faf
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-01-21 22:43:11 +0600

    Linked List

--- 02_LinkedList/OPP/index.php
+++ 02_LinkedList/OPP/index.php
@@ -2,17 +2,25 @@
 
 require_once '../OPP/PracticeWithLinkedList.php';
 
-$nodeObject = (new LinkedList())
-                // ->inserAtFirst('Introduction to PHP7')
-                // ->inserAtFirst('Mastering JavaScript')
+// $nodeObject = (new LinkedList())
+//                 // ->inserAtFirst('Introduction to PHP7')
+//                 // ->inserAtFirst('Mastering JavaScript')
 
-                ->insertAtLast('MySQL Workbench tutorial')
-                ->insertAtLast('Computer Fundamentals')
-                ->insertAtLast('PHP OPP')
-                ->insertAtLast('Database Management')
+//                 ->insertAtLast('MySQL Workbench tutorial')
+//                 ->insertAtLast('Computer Fundamentals')
+//                 ->insertAtLast('PHP OPP')
+//                 ->insertAtLast('Database Management')
 
-                // ->reverse()
+//                 // ->reverse()
 
-                // ->insertBefore('Working','PHP OPP')
-                ->display();
+//                 // ->insertBefore('Working','PHP OPP')
+//                 ->display();
 
+$obj = new LinkedList();
+$obj->inserAtFirst('woking 1');
+$obj->inserAtFirst('woking 2');
+$obj->inserAtFirst('woking 3');
+$obj->deleteLast();
+// $obj->deleteFirst();
+$obj->display();
+// echo "2nd Item is: ".$obj->getNthNode(1)->data;
