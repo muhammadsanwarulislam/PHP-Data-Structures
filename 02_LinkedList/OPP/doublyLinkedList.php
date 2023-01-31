ea7b2f2fa3cf58fbb38973bd1be43b969f7cfc09
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-01-31 21:43:46 +0600

    Working on doubly linked list

--- 02_LinkedList/OPP/doublyLinkedList.php
+++ 02_LinkedList/OPP/doublyLinkedList.php
@@ -2,5 +2,32 @@
 require_once '../OPP/node.php';
 
 class DoublyLinkedList {
-    
+    private $_firstNode = NULL;
+    private $_lastNode  = NULL;
+    private $_totalNode = 0;
+
+    public function insertAtFirst(string $data):DoublyLinkedList 
+    {
+        $newNode  = new NodeList($data);
+        if($this->_firstNode === NULL) {
+            $this->_firstNode   = &$newNode;
+            $this->_lastNode    = $newNode;
+        }else {
+            $currentNode        = $this->_firstNode;
+            $this->_firstNode   = &$newNode;
+            $newNode->next      = $currentNode;
+            $currentNode->prev  = $newNode;
+        }
+        return $this;
+    }
+
+    public function displayBackward() 
+    {
+        echo "Total Data : " . $this->_totalNode . "\n";
+        $currentNode = $this->_lastNode;
+        while ($currentNode !== NULL) {
+            echo $currentNode->data . "\n";
+            $currentNode = $currentNode->prev;
+        }
+    }
 }
--- 02_LinkedList/OPP/index.php
+++ 02_LinkedList/OPP/index.php
@@ -1,26 +1,27 @@
 <?php
 
 require_once '../OPP/singleLinkedList.php';
+require_once '../OPP/doublyLinkedList.php';
 
 //Using method chaining
-$nodeObject = (new SingleLinkedList())
-                ->inserAtFirst(3)
-                ->inserAtFirst(6)
-                ->inserAtFirst(2)
-                ->inserAtFirst(1)
-                ->inserAtFirst(4)
+// $nodeObject = (new SingleLinkedList())
+//                 ->inserAtFirst(3)
+//                 ->inserAtFirst(6)
+//                 ->inserAtFirst(2)
+//                 ->inserAtFirst(1)
+//                 ->inserAtFirst(4)
 
-                ->insertAtLast('MySQL Workbench tutorial')
-                ->insertAtLast('Computer Fundamentals')
-                ->insertAtLast('PHP OPP')
-                ->insertAtLast('Database Management')
+//                 ->insertAtLast('MySQL Workbench tutorial')
+//                 ->insertAtLast('Computer Fundamentals')
+//                 ->insertAtLast('PHP OPP')
+//                 ->insertAtLast('Database Management')
 
-                ->delete('PHP OPP')
-                ->reverse()
+//                 ->delete('PHP OPP')
+//                 ->reverse()
 
-                ->insertBefore('Working','PHP OPP')
-                ->insertionSortList()
-                ->display();
+//                 ->insertBefore('Working','PHP OPP')
+//                 ->insertionSortList()
+//                 ->display();
 
 // $obj = new SingleLinkedList();
 // $obj->inserAtFirst(4);
@@ -34,3 +35,13 @@ $nodeObject = (new SingleLinkedList())
 // $obj->insertionSortList();
 // $obj->display();
 // // echo "2nd Item is: ".$obj->getNthNode(1)->data;
+
+
+$nodeObject = (new DoublyLinkedList())
+                ->insertAtFirst(3)
+                ->insertAtFirst(6)
+                ->insertAtFirst(2)
+                ->insertAtFirst(1)
+                ->insertAtFirst(4)
+
+                ->displayBackward();
